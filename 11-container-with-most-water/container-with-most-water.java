099ac3030f587b0df3506f399203b0225616006d
VIVEKYADAV9569 <148001435+VIVEKYADAV9569@users.noreply.github.com>
2026-01-09 07:17:11 +0530

    Time: 5 ms (78.37%) | Memory: 77.6 MB (13.50%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -5,11 +5,12 @@ class Solution {
         int r=n-1;
         int max=0;
         while(l<r){
-            int current=Math.min(height[l],height[r])*(r-l);
-            max=Math.max(current,max);
-            if(height[l]<height[r])l++;
-            else{r--;}
+            int current=Math.min(height[r],height[l])*(r-l);
+            max=Math.max(max,current);
+            if(height[l]<height[r]){l++;}
+            else {r--;}
         }
-        return max;
+
+return max;
     }
 }
