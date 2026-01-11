3f07fe73b06245774236b6d2f607131cfa372de3
VIVEKYADAV9569 <148001435+VIVEKYADAV9569@users.noreply.github.com>
2026-01-11 12:37:43 +0530

    Time: 1 ms (99.85%) | Memory: 47 MB (32.65%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // https://github.com/VIVEKYADAV9569/DSA
-        int c = 0;
-        for (int i = 0; i < nums.length; i++) {
-            c ^= nums[i];
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            c=c^nums[i];
         }
         return c;
-
     }
 }
