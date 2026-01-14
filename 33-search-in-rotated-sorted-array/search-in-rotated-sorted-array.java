af175f21d9b81139ae34e802248d2d70461f7d99
VIVEKYADAV9569 <148001435+VIVEKYADAV9569@users.noreply.github.com>
2026-01-14 20:24:18 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (31.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -9,18 +9,21 @@ class Solution {
             }
             if(nums[start]<=nums[mid]){
                 if(target>=nums[start] && target<=nums[mid]){
-                    end=mid-1;
+                      end=mid-1;
                 }
                 else{
-                    start=mid+1;
+                 
+                     start=mid+1;
                 }
             }
             else{
-                if(target>=nums[mid]&& target<=nums[end]){
+                if(target>=nums[mid] && target<=nums[end]){
                     start=mid+1;
+                 
+
                 }
                 else{
-                    end=mid-1;
+                       end=mid-1;
                 }
             }
         }
